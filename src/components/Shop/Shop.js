4236eac6e220ba4452231cb16f427b2075689c22
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-29 01:43:27 +0600

    add products

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,12 +4,19 @@ import { useState } from 'react';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
     const first10 = fakeData.slice(0, 10);
-    
+    const [products, setProducts] = useState(first10);
+
     return (
         <div>
-            
+            <h1>This is Shop</h1>
+            <h3>{products.length}</h3>
+
+            <ul>
+                {
+                    products.map(product => <li>{product.name}</li>)
+                }
+            </ul>
         </div>
     );
 };
