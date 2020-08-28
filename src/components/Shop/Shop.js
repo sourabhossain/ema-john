d5ba7fd8dbcd4452cd2ea52e35908a13188f7255
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-29 01:40:19 +0600

    add hook

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import fakeData from '../../fakeData';
+import { useState } from 'react';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const first10 = fakeData.slice(0, 10);
+    
     return (
         <div>
-             
+            
         </div>
     );
 };
