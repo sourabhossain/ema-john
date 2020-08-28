c20b70293d4268563c2d9afb0e435779e1fa6e0c
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-29 01:57:32 +0600

    add two div

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,15 +8,18 @@ const Shop = () => {
     const [products, setProducts] = useState(first10);
 
     return (
-        <div>
-            <h1>This is Shop</h1>
-            <h3>{products.length}</h3>
-
-            <ul>
-                {
-                    products.map(product => <li>{product.name}</li>)
-                }
-            </ul>
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product => <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+            
+            <div className="cart-container">
+                <h1>This is cart</h1>
+            </div>
         </div>
     );
 };
