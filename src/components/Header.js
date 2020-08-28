741c492360c2a32c1c4cc94b660458c4b93370ec
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-29 01:13:27 +0600

    add nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,12 @@ const Header = () => {
         <div className="header">
             <img src={logo} alt="logo"/>
         </div>
+
+        <nav>
+            <a href="/shop">Shop</a>
+            <a href="/review">Order Review</a>
+            <a href="/manage">Manage Inventory</a>
+        </nav>
     );
 };
 
