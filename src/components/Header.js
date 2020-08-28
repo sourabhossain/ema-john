69eeea6a85e2868777fdcf80de8a725204b5bd7a
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-29 00:32:19 +0600

    add header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../images/logo.png'
+import logo from '../images/logo.png';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="logo" srcset="logo"/>
+        <div className="header">
+            <img src={logo} alt="logo"/>
         </div>
     );
 };
