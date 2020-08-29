e2f7e13581361005895a492fb5157644bec026bf
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-30 05:38:11 +0600

    add cart button

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,12 +7,16 @@ import Product from '../Product/Product'
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    
+    const handleAddProduct = (product) => {
+        console.log(product);
+    } 
 
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(product => <Product product={product}></Product>)
+                    products.map(product => <Product handleAddProduct={handleAddProduct} product={product}></Product>)
                 }
             </div>
             
