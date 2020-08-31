e72b7da7ddd14e03aa2fbe01fd411040341b4c40
Sourab Hossain <sourabhossain.fci@gmail.com>
2020-08-31 08:31:17 +0600

    cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
+    const total = cart.reduce((total, product) => total + product.price, 0);
+    let shipping = 0;
 
+    if (total >= 100) {
+        shipping = 0;
+    } else if (total >= 50) {
+        shipping = 25;
+    } else {
+        shipping = 50;
+    }
+    
     return (
         <div>
             <h4>Order Summary</h4>
             <p>Items Ordered: {cart.length}</p>
+            <p>Total Price: {total}</p>
+            <p>Shipping Price: {shipping}</p>
         </div>
     );
 };
